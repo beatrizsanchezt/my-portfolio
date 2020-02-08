5e74d6864877df01ab74c432d799c68ddc427cd0
12358-lab <12358-lab@protonmail.com>
2020-02-08 11:04:06 +0000

    Removing props

--- src/pages/blog/paragraph.js
+++ src/pages/blog/paragraph.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import { EuiTitle, EuiSpacer, EuiText } from "@elastic/eui";
 import "./blog.css";
 
-const Paragraph = ({ title, content }, props) => (
+const Paragraph = ({ title, content }) => (
   <Fragment>
     <EuiTitle size="s">
       <h3>{title}</h3>
