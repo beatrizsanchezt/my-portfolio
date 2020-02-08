a0e5a4fc59e3966f6cf2ffa207ddfcd79c4b93a1
12358-lab <12358-lab@protonmail.com>
2020-02-08 10:58:44 +0000

    Migratre to function component

--- src/pages/skills/skills.js
+++ src/pages/skills/skills.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   EuiPage,
   EuiPageBody,
@@ -12,37 +12,32 @@ import {
 import startIcon from "../../assets/images/start.png";
 import "./skills.css";
 
-class Skills extends Component {
-  constructor(props) {
-    super(props);
-    const skills = [
-      { name: "CSS", level: 2 },
-      { name: "HTML", level: 2 },
-      { name: "JavaScript", level: 2 },
-      { name: "React", level: 1 },
-      { name: "Automation", level: 1 },
-      { name: "GitHub", level: 2 },
-      { name: ".NET*", level: 3 },
-      { name: "SQL Server", level: 5 },
-      { name: "PL/SQL", level: 5 },
-      { name: "R", level: 1 },
-      { name: "Agile frameworks", level: 5 },
-      { name: "BDD", level: 4 },
-      { name: "Product owner skills", level: 4 },
-      { name: "Spanish", level: 5 },
-      { name: "English", level: 4 }
-    ];
-    this.state = {
-      sortField: "name",
-      sortDirection: "asc",
-      skills
-    };
-  }
+const Skills = () => {
+  const skillsArray = [
+    { name: "CSS", level: 2 },
+    { name: "HTML", level: 2 },
+    { name: "JavaScript", level: 2 },
+    { name: "React", level: 1 },
+    { name: "Automation", level: 1 },
+    { name: "GitHub", level: 2 },
+    { name: ".NET*", level: 3 },
+    { name: "SQL Server", level: 5 },
+    { name: "PL/SQL", level: 5 },
+    { name: "R", level: 1 },
+    { name: "Agile frameworks", level: 5 },
+    { name: "BDD", level: 4 },
+    { name: "Product owner skills", level: 4 },
+    { name: "Spanish", level: 5 },
+    { name: "English", level: 4 }
+  ];
 
-  compareValues = (key, order = "asc") => {
+  const [sortField, setSortField] = React.useState("name");
+  const [sortDirection, setsortDirection] = React.useState("asc");
+  const [skills] = React.useState(skillsArray);
+
+  const compareValues = (key, order = "asc") => {
     return function innerSort(a, b) {
       if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-        // property doesn't exist on either object
         return 0;
       }
 
@@ -59,77 +54,68 @@ class Skills extends Component {
     };
   };
 
-  onTableChange = ({ sort = {} }) => {
+  const onTableChange = ({ sort = {} }) => {
     const { field: sortField, direction: sortDirection } = sort;
-    const skills = this.state.skills;
-    skills.sort(this.compareValues(sortField, sortDirection));
-
-    this.setState({
-      sortField,
-      sortDirection
-    });
+    skills.sort(compareValues(sortField, sortDirection));
+    setSortField(sortField);
+    setsortDirection(sortDirection);
   };
 
-  render() {
-    const { sortField, sortDirection, skills } = this.state;
+  const sorting = {
+    sort: {
+      field: sortField,
+      direction: sortDirection
+    }
+  };
 
-    const sorting = {
-      sort: {
-        field: sortField,
-        direction: sortDirection
-      }
-    };
+  const getLevel = level => {
+    var starts = ["⭐"];
+    for (var i = 1; i < level; i++) {
+      starts.push("⭐");
+    }
+    return starts;
+  };
 
-    const getLevel = level => {
-      //var starts = [<EuiIcon size="xl" type={startIcon} />];
-      var starts = ['⭐'];
-      for (var i = 1; i < level; i++) {
-        starts.push('⭐');
-      }
-      return starts;
-    };
+  const columns = [
+    {
+      field: "name",
+      name: "Skill",
+      sortable: true,
+      header: true,
+      width: "20%"
+    },
+    {
+      field: "level",
+      name: "Level",
+      sortable: true,
+      header: true,
+      render: level => getLevel(level),
+      width: "40%"
+    }
+  ];
 
-    const columns = [
-      {
-        field: "name",
-        name: "Skill",
-        sortable: true,
-        header: true,
-        width: "20%"
-      },
-      {
-        field: "level",
-        name: "Level",
-        sortable: true,
-        header: true,
-        render: level => getLevel(level),
-        width: "40%"
-      }
-    ];
-
-    return (
-      <EuiPage>
-        <EuiPageBody>
-          <EuiPageHeader>
-            <EuiPageHeaderSection>
-              <EuiTitle size="l">
-                <h1>Skills</h1>
-              </EuiTitle>
-            </EuiPageHeaderSection>
-          </EuiPageHeader>
-          <EuiBasicTable
-            items={skills}
-            columns={columns}
-            sorting={sorting}
-            onChange={this.onTableChange}
-          />
-          <EuiText className={"footerNote"}>
-            <p>*Interested in develop other skills</p>
-          </EuiText>
-        </EuiPageBody>
-      </EuiPage>
-    );
-  }
-}
+  return (
+    <EuiPage>
+      <EuiPageBody>
+        <EuiPageHeader>
+          <EuiPageHeaderSection>
+            <EuiTitle size="l">
+              <h1>Skills</h1>
+            </EuiTitle>
+          </EuiPageHeaderSection>
+        </EuiPageHeader>
+        <EuiBasicTable
+          items={skills}
+          columns={columns}
+          sorting={sorting}
+          onChange={onTableChange}
+        />
+        <EuiText className={"footerNote"}>
+          <p>*Interested in develop other skills</p>
+        </EuiText>
+      </EuiPageBody>
+    </EuiPage>
+  );
+};
 
 export default Skills;
