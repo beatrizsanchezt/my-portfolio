824f3f034819f3740c988871cb05891a0e6bc11d
12358-lab <12358-lab@protonmail.com>
2020-02-09 20:13:50 +0000

    Removing Fragment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { About, Blog, Education, Layout, Projects, Skills } from "../src/pages";
 
 function App() {
   return (
-    <Fragment>
-      <Layout>
-        <Switch>
-          <Route path="/blog" component={Blog} />
-          <Route path="/education" component={Education} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/skills" component={Skills} />
-          <Route path="/" component={About} />
-        </Switch>
-      </Layout>
-    </Fragment>
+    <Layout>
+      <Switch>
+        <Route path="/blog" component={Blog} />
+        <Route path="/education" component={Education} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/" component={About} />
+      </Switch>
+    </Layout>
   );
 }
 
